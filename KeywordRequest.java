fd35b5ea37088fb86650361faeff2c561991bc03
Apeksha <163647983+apekshakumariitp@users.noreply.github.com>
2025-08-21 22:26:28 +0530

    Add files via upload

---
+++ KeywordRequest.java
@@ -0,0 +1,24 @@
+   package com.example.ai.dto;
+
+   public class KeywordRequest {
+       private String keyword;
+       private int weight;
+
+       // Getters and Setters
+       public String getKeyword() {
+           return keyword;
+       }
+
+       public void setKeyword(String keyword) {
+           this.keyword = keyword;
+       }
+
+       public int getWeight() {
+           return weight;
+       }
+
+       public void setWeight(int weight) {
+           this.weight = weight;
+       }
+   }
+   

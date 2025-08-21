33ba79410f6751d1c48372ad0f51cf2db77fc7bc
Apeksha <163647983+apekshakumariitp@users.noreply.github.com>
2025-08-21 22:28:26 +0530

    Add files via upload

---
+++ JobKeywordRepository.java
@@ -0,0 +1,8 @@
+   package com.example.ai.repo;
+
+   import com.example.ai.entity.JobKeyword;
+   import org.springframework.data.jpa.repository.JpaRepository;
+
+   public interface JobKeywordRepository extends JpaRepository<JobKeyword, Long> {
+   }
+   
